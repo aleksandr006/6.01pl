fe46644c3b333c8934d90b518dec859b806e9109
opilane <opilane@HP-CZC6297BBB>
2022-01-11 11:51:48 +0200

    sa

--- _5.01pl.py
+++ _5.01pl.py
@@ -66,38 +66,78 @@ def veel():
         uvokno.config(text="Увеличить окно")
         t=0
 def kittt():
-    x1=np.arange(0,9,0.5)
-    y1=(2/27)*x1*x1-3
-    x2=np.arange(-10,0,0.5)
-    y2=0.04*x2*x2-3
-    x3=np.arange(-9,-3,0.5)
+    x1=np.arange(0,9.5,0.5)
+    y1=(2/27)*x1*x1-3 
+    x2=np.arange(-10,0.5,0.5)
+    y2=0.04*x2*x2-3 
+    x3=np.arange(-9,-2.5,0.5)
     y3=(2/9)*(x3+6)**2+1
     x4=np.arange(-3,9.5,0.5)
-    y4=(-1/12)*(x4-3)**2+6
-    x5=np.arange(5,9.5,0.5)
+    y4=(-1/12)*(x4-3)**2+6 
+    x5=np.arange(5,9,0.5)
     y5=(1/9)*(x5-5)**2+2
-    x6=np.arange(5,9,0.5)
+    x6=np.arange(5,8.3,0.5)
     y6=(1/8)*(x6-7)**2+1.5
     x7=np.arange(-13,-8.5,0.5)
     y7=(-0.75)*(x7+11)**2+6
     x8=np.arange(-15,-12.5,0.5)
     y8=(-0.5)*(x8+13)**2+3
-    x9=np.arange(-15,-10,0.5)
+    x9=np.arange(-15,-9.5,0.5)
     y9=[1]*len(x9)
     x10=np.arange(3,4,0.5)
     y10=[3]*len(x10)
-    fig = plt.figure()
+    fig=plt.figure()
     plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10)
-    plt.title("Квадратное уравнение")
-    plt.ylabel("y")
-    plt.xlabel("x")
+    plt.title('square equation')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+       
+def o4ki():
+    x1=np.arange(-9,-1,0.5)
+    y1=(-1/16)*(x1+5)**2+2
+    x2=np.arange(1,9,0.5)
+    y2=(-1/16)*(x2-5)**2+2
+    x3=np.arange(-9,-1,0.5)
+    y3=1/4*(x3+5)**2-3
+    x4=np.arange(1,9,0.5)
+    y4=1/4*(x4-5)**2-3
+    x5=np.arange(-9,-6,0.5)
+    y5=-(x5+7)**2+5
+    x6=np.arange(6,9,0.5)
+    y6=-(x6-7)**2+5
+    x7=np.arange(-1,1,0.5)
+    y7=(-0.5)*x7*x7+1.5
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7)
+    plt.title('Очки')
+    plt.ylabel('y')
+    plt.xlabel('x')
     plt.grid(True)
     plt.show()
     
 def zont():
-    pass
-def laguska():
-    pass
+    x1=np.arange(-12,12,0.5)
+    y1=(-1/18)*x1*x1+12
+    x2=np.arange(-4,4,0.5)
+    y2=(-1/8)*x2*x2+6
+    x3=np.arange(-12,-4,0.5)
+    y3=(-1/8)*(x3+8)**2+6
+    x4=np.arange(4,12,0.5)
+    y4=(-1/8)*(x4-8)**2+6
+    x5=np.arange(-4,-0.3,0.5)
+    y5=2*(x5+3)**2-9
+    x6=np.arange(-4,0.2,0.5)
+    y6=1.5*(x6+3)**2-10
+    fig = plt.figure()
+    plt.plot(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6)
+    plt.title('Зонтик')
+    plt.ylabel('y')
+    plt.xlabel('x')
+    plt.grid(True)
+    plt.show()
+    
 
 
 aken=Tk()
@@ -117,8 +157,8 @@ kop=Entry(f1,font="Arial 20", fg="green",bg="lightblue",width=5)
 pop=Label(f1,text="=0",font="Arial 20", fg="green")
 kit=Button(f1,text="Решить",font="Arial 20",bg="green",command=lahenda)
 kot_g=Button(f1,text="График", font="Arial 20",bg="green",command=graafik)
-rad1=Radiobutton(f2,text="laguska", font="Arial 20",bg="green")
-rad2=Radiobutton(f2,text="zontik", font="Arial 20",bg="green")
+rad1=Radiobutton(f2,text="o4ki", font="Arial 20",bg="green",command=o4ki)
+rad2=Radiobutton(f2,text="zont", font="Arial 20",bg="green",command=zont)
 rad3=Radiobutton(f2,text="kit", font="Arial 20",bg="green",command=kittt)
 uvokno=Button(f2,text="uvel okno",font="Arial 20",bg="green",command=veel)
 
